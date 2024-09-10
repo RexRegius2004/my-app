194abf740471874c9bf7f113e8bfedca29f611c9
Reginald Misuela <134357506+RexRegius2004@users.noreply.github.com>
2024-09-10 20:32:18 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Greeting from './Greeting';
 
 function App() {
   return (
-   <div>
-    <h1>I love you sef</h1>
-    <p>I'll wait till the end of reality</p>
+   <div className="ProfileCard">
+    <style>
+
+    </style>
+    <Greeting name=" Name: Joshua Gabriel L. Flores"/>
+    <Greeting Age="Age: 21" />
+    <Greeting Description="About me:  Aspiring Game Developer"/>
+    <Greeting Location=" Location: Manila"/>
+
+    <div className='ProfileCard2'>
+    <Greeting name=" Name: John Phoenix"/>
+    <Greeting Age="Age: 30" />
+    <Greeting Description="About me:  Aspiring Software Engineer"/>
+    <Greeting Location=" Location: Los Angeles"/>
+
+    <div className='ProfileCard3'>
+    <Greeting name=" Name: Reyna Cruz"/>
+    <Greeting Age="Age: 30" />
+    <Greeting Description="About me:  Aspiring Web Designer"/>
+    <Greeting Location=" Location: San Francisco"/>
+    </div>
+    </div>
    </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Greeting.js
@@ -0,0 +1,17 @@
+import React from 'react' 
+function Greeting(props) {
+    return(
+       <><h1>{props.name}{props.Age}{props.Description}{props.Location}</h1>
+       </>
+
+    );
+}
+    export default Greeting;
+
+
+
+
+
+
+
+
